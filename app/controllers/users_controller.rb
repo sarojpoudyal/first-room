18f3fa0d5abf5b8be6d7785a931d968c67c46ce3
Saroj Sharma <saroj.webdeveloper@gmail.com>
2016-05-17 14:47:49 +0530

    added login and registration template and fixed the routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+
+    else
+
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  get 'register', to: 'users#new'
+  post 'register', to: 'users#create'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
 end
